103020092bb2400c886f171ca0560c01bc536704
jrfl <jrfl@users.noreply.github.com>
2020-05-31 12:56:52 +0000

    Create pandoraconstants.py

---
+++ razore/pandoraconstants.py
@@ -0,0 +1,18 @@
+tinkerToolsId = 0x1EB8
+pickAxeId  = 0x0E86
+tinkerGump = 949095101
+ingots = 0x1bf2
+rune = 0x1f14
+shovel = 0xf39
+sewingKit = 0x0F9D
+runebookGumpId = 1431013363
+
+gems = [
+    hex(x) for x in range(0x0f0f, 0x0f31)
+]
+for x in range(0x3192, 0x3199):
+    gems.append(x)
+
+regs = [
+    hex(x) for x in range(0x0f78, 0x0f91)
+]
